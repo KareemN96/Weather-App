e2ea9bf4c2eaceb4c232dc9014e96369177b4390
KareemN96 <kakakimo08@gmail.com>
2024-12-18 22:41:04 +0200

    Add Forecast Hours

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,8 @@ async function allData(callData) {
   dataContainer = allWeatherData;
   currentWeather(dataContainer.location, dataContainer.current);
   anotherDays(dataContainer.forecast.forecastday);
+  dayPerHours(dataContainer.forecast.forecastday[0].hour);
+  console.log(dataContainer.forecast.forecastday[0].hour);
 }
 allData("Alexandria");
 document.getElementById("search-bar").addEventListener("keyup", function (e) {
@@ -37,24 +39,36 @@ const months = [
 function currentWeather(loca, curr) {
   const date = new Date(curr.last_updated);
   let today = `<div class="col-lg ps-0 pe-0 ms-3 me-3 mb-4 gy-5">
-  <div class="head-content d-flex justify-content-between bg-info ps-3 pe-3 pt-3 pb-3 rounded-5">
-  <small>${days[date.getDay()]}</small>
-  <small>${date.getDate() + months[date.getMonth()]}</small>
+  <div class="head-content d-flex justify-content-between bg-info p-3 rounded-5">
+  <small class="text-white">${
+    days[date.getDay()]
+  }<i class="fa-regular fa-calendar-days fs-4 ms-2"></i></small>
+  <small class="text-white"><i class="fa-regular fa-calendar-days fs-4 me-2"></i>${
+    date.getDate() + " " + months[date.getMonth()]
+  }</small>
   </div>
-  <div class="row bg-dark-subtle m-0 rounded-5 p-2 mt-2 h-100">
-  <span class="col-12 mt-1">${loca.name}</span>
-  <span class="col-12 display-1 fw-bolder lh-base">${
+  <div class="row bg-info-subtle m-0 rounded-5 p-2 mt-2 h-100">
+  <span class="col-12 mt-1 text-primary"><i class="fa-solid fa-location-dot me-1"></i>${
+    loca.name
+  }</span>
+  <span class="col-12 display-1 fw-bolder lh-base text-primary">${
     curr.temp_c
   } <sup>o</sup>C</span>
   <img src="${
     curr.condition.icon
   }" class="col-12 w-auto fw-bolder img-fluid" alt="${curr.condition.text}">
   <span class="col-12 text-primary fw-normal">${curr.condition.text}</span>
-  <div class="footer-content col-12 d-flex">
+  <div class="footer-content col-12 d-flex text-primary">
   <span>
-                    <i class="fa-solid fa-umbrella me-2"></i>20%</span>
-                    <span><i class="fa-solid fa-wind ms-3 me-2"></i>18km/h</span>
-                    <span><i class="fa-solid fa-angles-right ms-3 me-2"></i>East</span>
+                    <i class="fa-solid fa-umbrella me-2"></i>${
+                      curr.cloud
+                    }%</span>
+                    <span><i class="fa-solid fa-wind ms-3 me-2"></i>${
+                      curr.wind_kph
+                    } km/h</span>
+                    <span><i class="fa-solid fa-angles-right ms-3 me-2"></i>${
+                      curr.wind_dir
+                    }</span>
                     </div>
                     </div>
                     </div>`;
@@ -66,21 +80,24 @@ function anotherDays(daysData) {
   for (let i = 1; i < daysData.length; i++) {
     const date = new Date(daysData[i].date);
     otherDays += `<div class="col-lg ps-0 pe-0 ms-3 me-3 mb-4 gy-5">
-                      <div
-                      class="head-content d-flex justify-content-center align-items-center bg-info-subtle ps-3 pe-3 pt-3 pb-3 rounded-5"
-                      >
-                      <small>${days[date.getDay()]}</small>
-                      </div>
+              <div class="head-content d-flex justify-content-between bg-info-subtle text-primary p-3 rounded-5">
+              <small>${
+                days[date.getDay()]
+              }<i class="fa-regular fa-calendar-days fs-4 ms-2"></i></small>
+              <small><i class="fa-regular fa-calendar-days fs-4 me-2"></i>${
+                date.getDate() + " " + months[date.getMonth()]
+              }</small>
+              </div>
                       <div class="row text-center bg-info m-0 rounded-5 p-2 mt-2 h-100">
                       <img src="${
                         daysData[i].day.condition.icon
                       }" class="col-12 w-auto fw-bolder m-auto img-fluid" alt="${
       daysData[i].day.condition.text
     }">
-                      <span class="col-12 fs-4 fw-bolder lh-base">${
+                      <span class="col-12 fs-4 fw-bolder text-white mt-auto">${
                         daysData[i].day.maxtemp_c
                       } <sup>o</sup>C</span>
-  <span class="col-12 fs-6 fw-light lh-base">${
+  <span class="col-12 fs-6 fw-light text-white">${
     daysData[i].day.mintemp_c
   } <sup>o</sup>C</span>
   <span class="col-12 text-primary fw-normal">${
@@ -91,4 +108,49 @@ function anotherDays(daysData) {
   `;
   }
   document.getElementById("weatherDisp").innerHTML += otherDays;
+}
+function dayPerHours(dhCurr) {
+  let dayHour = ``,
+    dayWind = ``;
+  for (let i = 0; i < dhCurr.length; i++) {
+    const d = new Date(dhCurr[i].time);
+    let h = d.getHours();
+    if (h > 11) {
+      h = h - 12 + " PM";
+      h = h.replace("0 PM", "12 PM");
+      h = h.replace("112 PM", "10 PM");
+    } else {
+      if (h === 0) h = 12;
+      h = h + " AM";
+    }
+    dayHour += `<div class="col-4">
+                    <div class="card text-center p-3 rounded-5 bg-transparent text-primary border-primary">
+                      <h6><i class="fa-regular fa-clock me-2"></i>${h}</h6>
+                      <img src="${dhCurr[i].condition.icon}" alt="${dhCurr[i].condition.text}" class="w-auto fw-bolder m-auto img-fluid">
+                      <small><i class="fa-solid fa-temperature-three-quarters me-1"></i>${dhCurr[i].temp_c}<sup>o</sup>C</small>
+                  </div>
+                </div>`;
+  }
+
+  for (let w = 0; w < dhCurr.length; w++) {
+    const d = new Date(dhCurr[w].time);
+    let h = d.getHours();
+    if (h > 11) {
+      h = h - 12 + " PM";
+      h = h.replace("0 PM", "12 PM");
+      h = h.replace("112 PM", "10 PM");
+    } else {
+      if (h === 0) h = 12;
+      h = h + " AM";
+    }
+    dayWind += `<div class="col-4">
+                  <div class="card text-center p-2 rounded-5 bg-transparent text-primary border-primary">
+                    <h6><i class="fa-regular fa-clock me-2"></i>${h}</h6>
+                    <img src="Imgs/navigation.png" alt="navigation" class="w-25 fw-bolder m-auto mt-3 mb-3 " style="transform: rotate(${dhCurr[w].wind_degree}deg);">
+                    <small><i class="fa-solid fa-wind me-2"></i>${dhCurr[w].wind_kph} km/h</small>
+                  </div>
+                </div>`;
+  }
+  document.querySelector("#hoursDay").innerHTML = dayHour;
+  document.querySelector("#windSpeed").innerHTML = dayWind;
 }
